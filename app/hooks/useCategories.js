d07e0e9bdb6d0000c17025072c6991ff98215b6f
Filobater Nader <81650664+filobater@users.noreply.github.com>
2024-06-21 18:28:07 +0300

    Update category link

--- app/hooks/useCategories.js
+++ app/hooks/useCategories.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 
 const getCategories = () => {
-  return axios.get('https://dummyjson.com/products/categories');
+  return axios.get('https://dummyjson.com/products/category-list');
 };
 
 export const useCategories = () => {
